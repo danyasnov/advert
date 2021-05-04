c11d95ce3e5e2b0c1283f5438638c3f1215216b4
danyasnov <yasnov.one@gmail.com>
2021-05-04 15:53:37 +0300

    Header 768-1360 Footer 360-768

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,8 +2,9 @@ const path = require('path')
 
 module.exports = {
   i18n: {
-    defaultLocale: 'ru',
+    defaultLocale: 'en',
     locales: ['en', 'el', 'ro', 'ru', 'tr', 'uk', 'cimode'],
+    localePath: path.resolve('./locales'),
+    fallbackLng: 'en',
   },
-  localePath: path.resolve('./locales'),
 }
