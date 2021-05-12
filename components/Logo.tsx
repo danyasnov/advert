94c400e1a9684cd0ce406909c6cd20c9456e9e87
danyasnov <yasnov.one@gmail.com>
2021-05-12 16:20:04 +0300

    fix logo component

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,12 +4,18 @@ import Icon from './Icon'
 const Logo: FC = () => {
   return (
     <div className='flex flex-col justify-center '>
-      <div className='l:hidden'>
-        <Icon type='icAdvertoLogoInverseSquare' width={40} height={40} />
-      </div>
-      <div className='hidden l:block'>
-        <Icon type='icAdvertoLogoInverseLandscape' width={146} height={32} />
-      </div>
+      <Icon
+        type='icAdvertoLogoInverseSquare'
+        width={40}
+        height={40}
+        className='l:hidden'
+      />
+      <Icon
+        type='icAdvertoLogoInverseLandscape'
+        width={146}
+        height={32}
+        className='hidden l:block'
+      />
     </div>
   )
 }
