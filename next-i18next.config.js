51f5f2e1e120f6e6257bc6d67298da49a4e3012f
danyasnov <yasnov.one@gmail.com>
2021-06-21 16:52:36 +0300

    locale based domain

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,35 +1,30 @@
 const path = require('path')
 
+const locales = ['en', 'el', 'ro', 'ru', 'tr', 'uk']
+const getDomains = (domain) => {
+  const result = []
+  locales.forEach((l) => {
+    if (l === 'en') {
+      result.push({domain, defaultLocale: l})
+    } else {
+      result.push({domain: `${l}.${domain}`, defaultLocale: l})
+    }
+  })
+  return result
+}
+
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'el', 'ro', 'ru', 'tr', 'uk'],
+    locales,
     fallbackLng: 'en',
     domains: [
       {
         domain: 'localhost',
         defaultLocale: 'ru',
       },
-      {
-        domain: 'adverto.sale',
-        defaultLocale: 'en',
-      },
-      {
-        domain: 'el.adverto.sale',
-        defaultLocale: 'el',
-      },
-      {
-        domain: 'ro.adverto.sale',
-        defaultLocale: 'ro',
-      },
-      {
-        domain: 'tr.adverto.sale',
-        defaultLocale: 'tr',
-      },
-      {
-        domain: 'uk.adverto.sale',
-        defaultLocale: 'uk',
-      },
+      ...getDomains('adverto.sale'),
+      ...getDomains('fpreprod.adverto.sale'),
     ],
   },
   localePath: path.resolve('./locales'),
