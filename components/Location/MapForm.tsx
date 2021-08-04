6a5097eeed53242c6b99133584182c713a51491c
danyasnov <yasnov.one@gmail.com>
2021-08-04 13:25:52 +0300

    AS-24

--- components/Location/MapForm.tsx
+++ components/Location/MapForm.tsx
@@ -4,6 +4,7 @@ import Slider from 'rc-slider'
 import {useTranslation} from 'next-i18next'
 import {parseCookies} from 'nookies'
 import {useRouter} from 'next/router'
+import IcAim from 'icons/material/Aim.svg'
 import SecondaryButton from '../Buttons/SecondaryButton'
 import PrimaryButton from '../Buttons/PrimaryButton'
 import {makeRequest} from '../../api'
@@ -14,7 +15,7 @@ import {
   setCookiesObject,
 } from '../../helpers'
 import {SerializedCookiesState} from '../../types'
-// import Autocomplete from '../Selects/Autocomplete'
+import Button from '../Buttons/Button'
 
 const getMark = (label) => ({
   style: {
@@ -59,6 +60,21 @@ const zoomMarksMap = {
 }
 
 const invertedMarksMap = objectFlip(marksMap)
+
+const getPosition = (): Promise<{
+  lat: number
+  lng: number
+}> => {
+  return new Promise((resolve, reject) =>
+    navigator.geolocation.getCurrentPosition((position) => {
+      resolve({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      })
+    }, reject),
+  )
+}
+
 interface Props {
   onClose: () => void
 }
@@ -132,42 +148,56 @@ const MapForm: FC<Props> = ({onClose}) => {
   return (
     <div className='flex flex-col justify-between h-full'>
       <div className='pt-4 px-4 s:px-6'>
-        <div style={{height: '288px'}} className='my-4 -mx-4 s:-mx-0'>
+        <div style={{height: '288px'}} className='my-4 -mx-4 s:-mx-0 relative'>
           {location && (
-            <GoogleMapReact
-              bootstrapURLKeys={{key: process.env.NEXT_PUBLIC_GOOGLE_API}}
-              center={location}
-              onChange={onChangeMap}
-              yesIWantToUseGoogleMapApiInternals
-              margin={[1, 2, 3, 4]}
-              defaultZoom={8}
-              onGoogleApiLoaded={({map, maps}) => {
-                mapRef.current = map
-                mapsRef.current = maps
-                circle.current = new maps.Circle({
-                  strokeColor: '#FF9514',
-                  strokeWeight: 2,
-                  fillColor: '#FF9514',
-                  fillOpacity: 0.2,
-                  map,
-                  center: location,
-                  radius: marksMap[radius] * 1000,
-                })
-                const svgMarker = {
-                  path: 'M14 39L13.6274 39.3334L14 39.7499L14.3726 39.3334L14 39ZM14 39C14.3726 39.3334 14.3727 39.3333 14.3729 39.3331L14.3734 39.3325L14.3754 39.3302L14.3829 39.3218L14.4117 39.2894C14.437 39.2608 14.4741 39.2187 14.5224 39.1635C14.6189 39.0532 14.7599 38.8906 14.9391 38.6804C15.2974 38.2602 15.8084 37.6495 16.4213 36.8855C17.6465 35.358 19.281 33.2152 20.9164 30.7547C22.5509 28.2956 24.1923 25.5102 25.4266 22.6982C26.6582 19.8922 27.5 17.025 27.5 14.4118C27.5 6.75161 21.4624 0.5 14 0.5C6.53761 0.5 0.5 6.75161 0.5 14.4118C0.5 17.025 1.34179 19.8922 2.57341 22.6982C3.80766 25.5102 5.44909 28.2956 7.08359 30.7547C8.719 33.2152 10.3535 35.358 11.5787 36.8855C12.1916 37.6495 12.7026 38.2602 13.0609 38.6804C13.2401 38.8906 13.3811 39.0532 13.4776 39.1635C13.5259 39.2187 13.563 39.2608 13.5883 39.2894L13.6171 39.3218L13.6246 39.3302L13.6266 39.3325L13.6271 39.3331C13.6273 39.3333 13.6274 39.3334 14 39Z',
-                  fillColor: '#FF9514',
-                  fillOpacity: 1,
-                  strokeColor: '#FFFFFF',
-                  strokeOpacity: 1,
-                  anchor: new maps.Point(14, 35),
-                }
-                marker.current = new maps.Marker({
-                  position: map.getCenter(),
-                  icon: svgMarker,
-                  map,
-                })
-              }}
-            />
+            <>
+              <GoogleMapReact
+                bootstrapURLKeys={{key: process.env.NEXT_PUBLIC_GOOGLE_API}}
+                center={location}
+                onChange={onChangeMap}
+                yesIWantToUseGoogleMapApiInternals
+                margin={[1, 2, 3, 4]}
+                defaultZoom={8}
+                onGoogleApiLoaded={({map, maps}) => {
+                  mapRef.current = map
+                  mapsRef.current = maps
+                  circle.current = new maps.Circle({
+                    strokeColor: '#FF9514',
+                    strokeWeight: 2,
+                    fillColor: '#FF9514',
+                    fillOpacity: 0.2,
+                    map,
+                    center: location,
+                    radius: marksMap[radius] * 1000,
+                  })
+                  const svgMarker = {
+                    path: 'M14 39L13.6274 39.3334L14 39.7499L14.3726 39.3334L14 39ZM14 39C14.3726 39.3334 14.3727 39.3333 14.3729 39.3331L14.3734 39.3325L14.3754 39.3302L14.3829 39.3218L14.4117 39.2894C14.437 39.2608 14.4741 39.2187 14.5224 39.1635C14.6189 39.0532 14.7599 38.8906 14.9391 38.6804C15.2974 38.2602 15.8084 37.6495 16.4213 36.8855C17.6465 35.358 19.281 33.2152 20.9164 30.7547C22.5509 28.2956 24.1923 25.5102 25.4266 22.6982C26.6582 19.8922 27.5 17.025 27.5 14.4118C27.5 6.75161 21.4624 0.5 14 0.5C6.53761 0.5 0.5 6.75161 0.5 14.4118C0.5 17.025 1.34179 19.8922 2.57341 22.6982C3.80766 25.5102 5.44909 28.2956 7.08359 30.7547C8.719 33.2152 10.3535 35.358 11.5787 36.8855C12.1916 37.6495 12.7026 38.2602 13.0609 38.6804C13.2401 38.8906 13.3811 39.0532 13.4776 39.1635C13.5259 39.2187 13.563 39.2608 13.5883 39.2894L13.6171 39.3218L13.6246 39.3302L13.6266 39.3325L13.6271 39.3331C13.6273 39.3333 13.6274 39.3334 14 39Z',
+                    fillColor: '#FF9514',
+                    fillOpacity: 1,
+                    strokeColor: '#FFFFFF',
+                    strokeOpacity: 1,
+                    anchor: new maps.Point(14, 35),
+                  }
+                  marker.current = new maps.Marker({
+                    position: map.getCenter(),
+                    icon: svgMarker,
+                    map,
+                  })
+                }}
+              />
+              <Button
+                className='bg-white w-10 h-10 absolute right-2.5 top-20 rounded'
+                onClick={async () => {
+                  try {
+                    const center = await getPosition()
+                    onChangeMap({center})
+                  } catch (e) {
+                    console.error(e)
+                  }
+                }}>
+                <IcAim className='fill-current text-black-c w-6 h-6' />
+              </Button>
+            </>
           )}
         </div>
         <Slider
