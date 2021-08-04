cb67edea6f6e0632a07d3b90794ca493d30571dc
danyasnov <yasnov.one@gmail.com>
2021-08-04 14:38:11 +0300

    AS-56

---
+++ pages/[country]/index.tsx
@@ -0,0 +1,25 @@
+import {GetServerSideProps} from 'next'
+import {getQueryValue, processCookies} from '../../helpers'
+import {fetchProductDetails} from '../../api/v2'
+
+export default function Home() {
+  return null
+}
+
+// @ts-ignore
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const {query, res} = ctx
+  const state = await processCookies(ctx)
+  const hash = getQueryValue(query, 'country')
+
+  const response = await fetchProductDetails(state, hash)
+
+  if (response.result) {
+    const {url} = response.result.advert
+    res.setHeader('location', url)
+    res.statusCode = 302
+    res.end()
+    return
+  }
+  throw new Error("can't find advert")
+}
