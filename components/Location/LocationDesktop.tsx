1c80f99400db59a977ed13b1ee0adb7d912a79f0
danyasnov <yasnov.one@gmail.com>
2021-08-24 09:31:59 +0300

    fix z-index header

--- components/Location/LocationDesktop.tsx
+++ components/Location/LocationDesktop.tsx
@@ -24,7 +24,7 @@ const LocationDesktop: FC<Props> = ({isOpen, onClose}) => {
       ariaHideApp={false}
       contentLabel='Location Modal'
       className='absolute rounded-6 overflow-hidden w-480px h-680px bg-white-a inset-x-0 mx-auto top-24 flex outline-none'
-      overlayClassName='fixed inset-0 bg-shadow-overlay max-h-screen overflow-y-auto'>
+      overlayClassName='fixed inset-0 bg-shadow-overlay max-h-screen overflow-y-auto z-20'>
       <div className='flex flex-col w-full'>
         <div className='px-6 mt-6 pb-4 flex justify-between border-b border-shadow-b'>
           <span className='text-h-2 text-black-b font-bold'>
