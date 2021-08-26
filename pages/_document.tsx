2e74f37f792c2f94a4b07456a270a340df1e22a7
danyasnov <yasnov.one@gmail.com>
2021-08-26 17:40:44 +0300

    AS-156 yandex metrika seo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -74,6 +74,48 @@ class MyDocument extends Document {
             gtag('config', 'UA-131255061-3');`),
             }}
           />
+          <script
+            type='text/javascript'
+            dangerouslySetInnerHTML={{
+              __html: JSON.stringify(`(function (d, w, c) {
+  (w[c] = w[c] || []).push(function () {
+    try {
+      w.yaCounter46964940 = new Ya.Metrika({
+        id: 46964940,
+        clickmap: true,
+        trackLinks: true,
+        accurateTrackBounce: true,
+        webvisor: true,
+        trackHash: true,
+        ecommerce: 'dataLayer',
+      })
+    } catch (e) {}
+  })
+  const n = d.getElementsByTagName('script')[0]
+  const s = d.createElement('script')
+  const f = function () {
+    n.parentNode.insertBefore(s, n)
+  }
+  s.type = 'text/javascript'
+  s.async = true
+  s.src = 'https://cdn.jsdelivr.net/npm/yandex-metrica-watch/watch.js'
+  if (w.opera == '[object Opera]') {
+    d.addEventListener('DOMContentLoaded', f, false)
+  } else {
+    f()
+  }
+})(document, window, 'yandex_metrika_callbacks')`),
+            }}
+          />
+          <noscript>
+            <div>
+              <img
+                src='https://mc.yandex.ru/watch/46964940'
+                style={{position: 'absolute', left: '-9999px'}}
+                alt=''
+              />
+            </div>
+          </noscript>
         </Head>
         <body>
           <Main />
