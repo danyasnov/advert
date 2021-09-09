3fb436c1d849335c17dd328a0c55991c6fa4b2a1
danyasnov <yasnov.one@gmail.com>
2021-09-09 13:22:28 +0300

    test firebase links

--- pages/b/[hash].tsx
+++ pages/b/[hash].tsx
@@ -2,6 +2,7 @@ import {GetServerSideProps} from 'next'
 import {getQueryValue} from '../../helpers'
 import {fetchFirebaseLink} from '../../api/db'
 import {CoverLink} from '../../types'
+import config from '../../config.json'
 
 export default function Home() {
   return null
@@ -12,7 +13,12 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const link = (await fetchFirebaseLink(
     getQueryValue(query, 'hash'),
   )) as CoverLink[]
-  console.log('fetchFirebaseLink', getQueryValue(query, 'hash'), link)
+  console.log(
+    'fetchFirebaseLink',
+    getQueryValue(query, 'hash'),
+    link,
+    config.host,
+  )
 
   return {
     redirect: {
