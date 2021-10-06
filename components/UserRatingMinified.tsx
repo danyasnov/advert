2308718189e5fb1bffa7f90aa87891007cbc40a0
danyasnov <yasnov.one@gmail.com>
2021-10-06 11:46:46 +0300

    AS-215 Нужно отображать только первую цифру после запятой в рейтинге

--- components/UserRatingMinified.tsx
+++ components/UserRatingMinified.tsx
@@ -12,7 +12,7 @@ const UserRatingMinified: FC<Props> = ({ratingMark, ratingMarkCnt}) => {
   return (
     <div className='text-brand-a1 text-body-2 flex items-center mb-2'>
       <IcStar className='fill-current h-6 w-6 mr-2' />
-      <span className='mr-1'>{calculated}</span>
+      <span className='mr-1'>{Math.round(calculated * 10) / 10}</span>
       <span>({ratingMarkCnt})</span>
     </div>
   )
