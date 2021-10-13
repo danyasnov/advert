8e3af5c5e3d4d8b234056fc3f65c1db2a558210a
danyasnov <yasnov.one@gmail.com>
2021-10-13 11:49:17 +0300

    AS-270 Решить проблему с символами в переводах

--- fetchTranslations.js
+++ fetchTranslations.js
@@ -1,5 +1,5 @@
-const https = require('https') // or 'https' for https:// URLs
 const fs = require('fs')
+const axios = require('axios')
 
 const token = 'proj_pub_InQLPlQ9dTs6LjID1IbA8Q'
 const getUrl = ({id, locale}) => {
@@ -14,40 +14,29 @@ const locales = [
   {id: 848367, locale: 'uk'},
 ]
 
-https.get(getUrl({id: 848359, locale: 'en'}), (resEn) => {
-  let bodyEn = ''
+const fetchTranslations = async () => {
+  const enRes = await axios.get(getUrl({id: 848359, locale: 'en'}))
+  const en = Object.fromEntries(
+    Object.entries(enRes.data).map(([key, val]) =>
+      val === null ? [key, key] : [key, val],
+    ),
+  )
+  fs.writeFileSync(`locales/en/common.json`, JSON.stringify(en, null, 2))
 
-  resEn.on('data', (chunk) => {
-    bodyEn += chunk
-  })
-
-  resEn.on('end', () => {
-    const en = Object.fromEntries(
-      Object.entries(JSON.parse(bodyEn)).map(([key, val]) =>
-        val === null ? [key, key] : [key, val],
+  // eslint-disable-next-line no-restricted-syntax
+  for (const localeData of locales) {
+    // eslint-disable-next-line no-await-in-loop
+    const currentLocaleRes = await axios.get(getUrl(localeData))
+    const formatted = Object.fromEntries(
+      Object.entries(currentLocaleRes.data).map(([key, val]) =>
+        val === null ? [key, en[key] ? en[key] : key] : [key, val],
       ),
     )
-    fs.writeFileSync(`locales/en/common.json`, JSON.stringify(en, null, 2))
-    locales.forEach((localeData) => {
-      https.get(getUrl(localeData), (res) => {
-        let body = ''
+    fs.writeFileSync(
+      `locales/${localeData.locale}/common.json`,
+      JSON.stringify(formatted, null, 2),
+    )
+  }
+}
 
-        res.on('data', (chunk) => {
-          body += chunk
-        })
-
-        res.on('end', () => {
-          const formatted = Object.fromEntries(
-            Object.entries(JSON.parse(body)).map(([key, val]) =>
-              val === null ? [key, en[key] ? en[key] : key] : [key, val],
-            ),
-          )
-          fs.writeFileSync(
-            `locales/${localeData.locale}/common.json`,
-            JSON.stringify(formatted, null, 2),
-          )
-        })
-      })
-    })
-  })
-})
+fetchTranslations()
