dce23865eee89bad012556133e23b2597b7e7b17
danyasnov <yasnov.one@gmail.com>
2021-10-21 14:11:48 +0300

    fix bugs

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -88,20 +88,19 @@ const Footer: FC = observer(() => {
                 <div className='flex flex-wrap items-center'>
                   <a
                     href='https://play.google.com/store/apps/details?id=adverto.sale&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'
-                    className='l:mr-2 l:mb-2 '
-                    style={{width: '135px', height: '40px'}}>
+                    className='l:mr-2 inline-flex m:-ml-2'
+                    style={{width: '135px', height: '55px'}}>
                     <img
                       alt='Get it on Google Play'
                       src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'
                     />
                   </a>
                   <a
-                    className='p-2 mt-2'
+                    className='inline-flex'
                     href='https://apps.apple.com/ru/app/adverto/id1287862488?itsct=apps_box_badge&amp;itscg=30200'
                     style={{
                       borderRadius: '13px',
                       width: '130px',
-                      height: '52px',
                     }}>
                     <img
                       src='https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-us?size=250x83&amp;releaseDate=1513555200&h=328ae9fa89f9b000ff0df1a0bd814025'
