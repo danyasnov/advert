a53fdb88af899bc81ffd3c7d7912767be0d0c1fb
danyasnov <yasnov.one@gmail.com>
2022-01-11 17:57:31 +0300

    fix location search

--- pages/api/location-text-search.ts
+++ pages/api/location-text-search.ts
@@ -1,15 +1,21 @@
 import type {NextApiRequest, NextApiResponse} from 'next'
 import {makeRequest} from '../../api'
+import {processCookies} from '../../helpers'
 
-export default (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+): Promise<void> => {
   const {query} = req
+  const state = await processCookies({req})
+
   return makeRequest({
     method: 'get',
     url: 'https://maps.googleapis.com/maps/api/place/textsearch/json',
     params: {
       key: process.env.GOOGLE_API,
       input: query.query,
-      language: 'ru',
+      language: state.language,
     },
   }).then(({data}) => {
     res.json(data)
