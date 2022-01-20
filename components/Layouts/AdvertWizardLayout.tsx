8638408963ff628b69babf7d6b74cfe82a8b45c7
danyasnov <yasnov.one@gmail.com>
2022-01-20 13:06:26 +0300

    fix min height

--- components/Layouts/AdvertWizardLayout.tsx
+++ components/Layouts/AdvertWizardLayout.tsx
@@ -12,7 +12,7 @@ const CategoriesLayout: FC = observer(() => {
     <>
       <Header />
       <MetaTags title={t('NEW_AD')} />
-      <div className='bg-white s:px-8 m:px-10 l:px-29 s:pb-44 flex min-h-screen s:min-h-9/10  flex l:justify-center'>
+      <div className='bg-white s:px-8 m:px-10 l:px-29 s:pb-44 flex min-h-9/10  flex l:justify-center'>
         <div className='flex w-full l:w-1208px'>
           <AdvertWizard />
         </div>
