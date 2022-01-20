de41bb5a6e75b2b2b5274310ee6af280220462f8
danyasnov <yasnov.one@gmail.com>
2022-01-20 12:54:06 +0300

    improve mobile select

--- components/Selects/MobileSelect.tsx
+++ components/Selects/MobileSelect.tsx
@@ -5,8 +5,11 @@ import IcArrowDropDown from 'icons/material/ArrowDropDown.svg'
 import {isArray, isEmpty, size} from 'lodash'
 import {FixedSizeList} from 'react-window'
 import IcCheck from 'icons/material/Check.svg'
+import IcSearch from 'icons/material/Search.svg'
 import {SelectProps} from './Select'
 import Button from '../Buttons/Button'
+import SecondaryButton from '../Buttons/SecondaryButton'
+import PrimaryButton from '../Buttons/PrimaryButton'
 
 const MobileSelect: FC<SelectProps> = ({
   options,
@@ -19,15 +22,24 @@ const MobileSelect: FC<SelectProps> = ({
   isMulti,
   id,
   isInvalid,
-  components,
 }) => {
   const {t} = useTranslation()
   const [open, setOpen] = useState(false)
+  const [filtered, setFiltered] = useState(options)
+  const [filter, setFilter] = useState('')
+
+  const onClose = () => {
+    setFilter('')
+    setFiltered(options)
+    setOpen(false)
+  }
 
   return (
     <div>
       <Button
-        className='border border-shadow-b h-10 rounded-lg w-full'
+        className={`border h-10 rounded-lg w-full ${
+          isInvalid ? 'border-error' : 'border-shadow-b'
+        }`}
         onClick={() => setOpen(true)}>
         <div className='flex justify-between items-center w-full px-3.5'>
           {isEmpty(value) ? (
@@ -47,44 +59,70 @@ const MobileSelect: FC<SelectProps> = ({
       <BottomSheet
         open={open}
         onDismiss={() => {
-          setOpen(false)
+          onClose()
         }}
         snapPoints={({maxHeight}) => maxHeight - 40}>
-        <div className='flex flex-col items-center justify-center w-full '>
+        <div className='flex flex-col items-center justify-center w-full pb-20'>
           <h3 className='text-h-3 font-medium text-nc-title mb-6 px-4'>
             {placeholder}
           </h3>
-          <div className='w-full'>
+          {isSearchable && (
+            <div className='w-full px-4 relative'>
+              <IcSearch className='w-6 h-6 absolute top-3 left-5 text-nc-icon' />
+              <input
+                className='w-full h-12 border border-nc-border flex rounded-lg py-4 pr-4 pl-8 text-nc-title'
+                placeholder={t('SEARCH')}
+                value={filter}
+                onChange={({target}) => {
+                  setFilter(target.value)
+                  setFiltered(
+                    options.filter((o) => {
+                      return o.label
+                        .toLowerCase()
+                        .includes(target.value.toLowerCase())
+                    }),
+                  )
+                }}
+              />
+            </div>
+          )}
+          <div className='w-full '>
             <FixedSizeList
               height={896}
-              itemCount={size(options)}
+              itemCount={size(filtered)}
               itemSize={48}
               width='100%'>
               {({index, style}) => (
                 <div style={style}>
                   <Button
-                    key={options[index].value}
-                    className='w-full px-4 border-b border-nc-border h-12 '
+                    // @ts-ignore
+                    disabled={filtered[index].disabled}
+                    key={filtered[index].value}
+                    className={`w-full px-4 border-b border-nc-border h-12 ${
+                      // @ts-ignore
+                      filtered[index].disabled ? 'text-nc-secondary-text' : ''
+                    }`}
                     onClick={() => {
+                      // debugger
                       if (isMulti) {
                         // @ts-ignore
-                        const filtered = value.filter(
-                          (v) => v.value !== options[index].value,
+                        const newFiltered = value.filter(
+                          (v) => v.value !== filtered[index].value,
                         )
-                        if (size(value) !== size(filtered)) {
-                          onChange(filtered)
+                        if (size(value) !== size(newFiltered)) {
+                          onChange(newFiltered)
                         } else {
                           // @ts-ignore
-                          onChange([...value, options[index]])
+                          onChange([...value, filtered[index]])
                         }
                       } else {
-                        onChange(options[index])
-                        setOpen(false)
+                        onChange(filtered[index])
+                        onClose()
                       }
                     }}>
                     <div className='w-full flex items-center justify-between'>
                       <span className='text-body-1 text-nc-text-primary'>
-                        {options[index].label}
+                        {filtered[index].label}
                       </span>
                       {isMulti && (
                         <>
@@ -94,19 +132,17 @@ const MobileSelect: FC<SelectProps> = ({
                             checked={
                               // @ts-ignore
                               !!value.find(
-                                (v) => v.value === options[index].value,
+                                (v) => v.value === filtered[index].value,
                               )
                             }
                             className='opacity-0 absolute h-4.5 w-4.5 cursor-pointer'
                           />
-                          <div
-                            className='bg-white border-2 rounded border-black-d h-4.5 w-4.5 flex
-       shrink-0 justify-center items-center mr-2'>
+                          <div className='bg-white border-2 rounded border-black-d h-4.5 w-4.5 flex shrink-0 justify-center items-center mr-2'>
                             <IcCheck className='fill-current text-black-c h-4.5 w-4.5 hidden' />
                           </div>
                         </>
                       )}
-                      {options[index].value === value?.value && !isMulti && (
+                      {filtered[index].value === value?.value && !isMulti && (
                         <IcCheck className='fill-current text-nc-primary h-4 w-4' />
                       )}
                     </div>
@@ -115,6 +151,23 @@ const MobileSelect: FC<SelectProps> = ({
               )}
             </FixedSizeList>
           </div>
+          {isMulti && (
+            <div className='h-20 flex w-full fixed bottom-0 p-4 space-x-2 bg-white'>
+              <SecondaryButton
+                className='w-full h-full'
+                onClick={() => {
+                  setFilter('')
+                  setFiltered(options)
+                  // @ts-ignore
+                  onChange([])
+                }}>
+                {t('RESET')}
+              </SecondaryButton>
+              <PrimaryButton className='w-full' onClick={onClose}>
+                {t('DONE')}
+              </PrimaryButton>
+            </div>
+          )}
         </div>
       </BottomSheet>
     </div>
