c4a6b62cbcccd6a9fc23925aa3f8ba1e21899968
danyasnov <yasnov.one@gmail.com>
2022-01-20 19:51:14 +0300

    fix selects

--- components/Selects/MobileSelect.tsx
+++ components/Selects/MobileSelect.tsx
@@ -3,7 +3,6 @@ import {BottomSheet} from 'react-spring-bottom-sheet'
 import {useTranslation} from 'next-i18next'
 import IcArrowDropDown from 'icons/material/ArrowDropDown.svg'
 import {isArray, isEmpty, size} from 'lodash'
-import {FixedSizeList} from 'react-window'
 import IcCheck from 'icons/material/Check.svg'
 import IcSearch from 'icons/material/Search.svg'
 import {SelectProps} from './Select'
@@ -61,95 +60,87 @@ const MobileSelect: FC<SelectProps> = ({
         onDismiss={() => {
           onClose()
         }}
-        snapPoints={({maxHeight}) => maxHeight - 40}>
-        <div className='flex flex-col items-center justify-center w-full pb-20'>
-          <h3 className='text-h-3 font-medium text-nc-title mb-6 px-4'>
-            {placeholder}
-          </h3>
-          {isSearchable && (
-            <div className='w-full px-4 relative'>
-              <IcSearch className='w-6 h-6 absolute top-3 left-5 text-nc-icon' />
-              <input
-                className='w-full h-12 border border-nc-border flex rounded-lg py-4 pr-4 pl-8 text-nc-title'
-                placeholder={t('SEARCH')}
-                value={filter}
-                onChange={({target}) => {
-                  setFilter(target.value)
-                  setFiltered(
-                    options.filter((o) => {
-                      return o.label
-                        .toLowerCase()
-                        .includes(target.value.toLowerCase())
-                    }),
-                  )
-                }}
-              />
-            </div>
-          )}
-          <div className='w-full '>
-            <FixedSizeList
-              height={896}
-              itemCount={size(filtered)}
-              itemSize={48}
-              width='100%'>
-              {({index, style}) => (
-                <div style={style}>
-                  <Button
+        snapPoints={({minHeight}) => minHeight}>
+        <div className='flex flex-col items-center justify-center w-full'>
+          <div className='fixed top-5 bg-white w-full flex flex-col items-center'>
+            <h3 className='text-h-3 font-medium text-nc-title mb-2 px-4'>
+              {placeholder}
+            </h3>
+            {isSearchable && (
+              <div className='w-full px-4 relative'>
+                <IcSearch className='w-6 h-6 absolute top-3 left-5 text-nc-icon' />
+                <input
+                  className='w-full h-12 border border-nc-border flex rounded-lg py-4 pr-4 pl-8 text-nc-title'
+                  placeholder={t('SEARCH')}
+                  value={filter}
+                  onChange={({target}) => {
+                    setFilter(target.value)
+                    setFiltered(
+                      options.filter((o) => {
+                        return o.label
+                          .toLowerCase()
+                          .includes(target.value.toLowerCase())
+                      }),
+                    )
+                  }}
+                />
+              </div>
+            )}
+          </div>
+          <div
+            className={`w-full flex flex-col mb-10 ${
+              isSearchable ? 'mt-20' : 'mt-10'
+            }`}>
+            {filtered.map((f) => (
+              <Button
+                // @ts-ignore
+                disabled={f.disabled}
+                key={f.value}
+                className={`w-full px-4 border-b border-nc-border h-12 ${
+                  // @ts-ignore
+                  f.disabled ? 'text-nc-secondary-text' : ''
+                }`}
+                onClick={() => {
+                  if (isMulti) {
                     // @ts-ignore
-                    disabled={filtered[index].disabled}
-                    key={filtered[index].value}
-                    className={`w-full px-4 border-b border-nc-border h-12 ${
+                    const newFiltered = value.filter((v) => v.value !== f.value)
+                    if (size(value) !== size(newFiltered)) {
+                      onChange(newFiltered)
+                    } else {
                       // @ts-ignore
-                      filtered[index].disabled ? 'text-nc-secondary-text' : ''
-                    }`}
-                    onClick={() => {
-                      // debugger
-                      if (isMulti) {
-                        // @ts-ignore
-                        const newFiltered = value.filter(
-                          (v) => v.value !== filtered[index].value,
-                        )
-                        if (size(value) !== size(newFiltered)) {
-                          onChange(newFiltered)
-                        } else {
+                      onChange([...value, f])
+                    }
+                  } else {
+                    onChange(f)
+                    onClose()
+                  }
+                }}>
+                <div className='w-full flex items-center justify-between'>
+                  <span className='text-body-1 text-nc-text-primary'>
+                    {f.label}
+                  </span>
+                  {isMulti && (
+                    <>
+                      <input
+                        type='checkbox'
+                        readOnly
+                        checked={
                           // @ts-ignore
-                          onChange([...value, filtered[index]])
+                          !!value.find((v) => v.value === f.value)
                         }
-                      } else {
-                        onChange(filtered[index])
-                        onClose()
-                      }
-                    }}>
-                    <div className='w-full flex items-center justify-between'>
-                      <span className='text-body-1 text-nc-text-primary'>
-                        {filtered[index].label}
-                      </span>
-                      {isMulti && (
-                        <>
-                          <input
-                            type='checkbox'
-                            readOnly
-                            checked={
-                              // @ts-ignore
-                              !!value.find(
-                                (v) => v.value === filtered[index].value,
-                              )
-                            }
-                            className='opacity-0 absolute h-4.5 w-4.5 cursor-pointer'
-                          />
-                          <div className='bg-white border-2 rounded border-black-d h-4.5 w-4.5 flex shrink-0 justify-center items-center mr-2'>
-                            <IcCheck className='fill-current text-black-c h-4.5 w-4.5 hidden' />
-                          </div>
-                        </>
-                      )}
-                      {filtered[index].value === value?.value && !isMulti && (
-                        <IcCheck className='fill-current text-nc-primary h-4 w-4' />
-                      )}
-                    </div>
-                  </Button>
+                        className='opacity-0 absolute h-4.5 w-4.5 cursor-pointer'
+                      />
+                      <div className='bg-white border-2 rounded border-black-d h-4.5 w-4.5 flex shrink-0 justify-center items-center mr-2'>
+                        <IcCheck className='fill-current text-black-c h-4.5 w-4.5 hidden' />
+                      </div>
+                    </>
+                  )}
+                  {f.value === value?.value && !isMulti && (
+                    <IcCheck className='fill-current text-nc-primary h-4 w-4' />
+                  )}
                 </div>
-              )}
-            </FixedSizeList>
+              </Button>
+            ))}
           </div>
           {isMulti && (
             <div className='h-20 flex w-full fixed bottom-0 p-4 space-x-2 bg-white'>
