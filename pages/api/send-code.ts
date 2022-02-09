bb8be89be327fafea44f355bde4d9ab02ab56e04
danyasnov <yasnov.one@gmail.com>
2022-02-09 11:27:57 +0300

    change endpoint send sms

--- pages/api/send-code.ts
+++ pages/api/send-code.ts
@@ -1,9 +1,16 @@
 import type {NextApiRequest, NextApiResponse} from 'next'
-import {sendCode} from '../../api/v1'
+import {AuthType} from 'front-api/src/models'
+import {API_URL, makeRequest} from '../../api'
 
 export default (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   const {body} = req
-  return sendCode(body.type, body.incoming).then((result) => {
-    return res.json(result)
+  const {type, incoming} = body
+  const params = type === AuthType.phone ? {num: incoming} : {email: incoming}
+  return makeRequest({
+    data: `query=${JSON.stringify(params)}`,
+    method: 'post',
+    url: `${API_URL}/v1/sendAuthSmsUser_2390cf0ufjdsklfjmckas.php`,
+  }).then((response) => {
+    return res.json(response.data)
   })
 }
