3156b48cf06069c7f30ab21cf9dd628cfaae9008
danyasnov <yasnov.one@gmail.com>
2022-03-29 17:47:46 +0300

    safety landing

--- components/Layouts/SafetyLayout.tsx
+++ components/Layouts/SafetyLayout.tsx
@@ -4,6 +4,15 @@ import {observer} from 'mobx-react-lite'
 import IcPeople from 'icons/safety-landing/People.svg'
 import IcLanguage from 'icons/safety-landing/Language.svg'
 import IcVerified from 'icons/safety-landing/Verified.svg'
+import IcImportant from 'icons/safety-landing/Important.svg'
+import IcDeliveryFast from 'icons/safety-landing/DeliveryFast.svg'
+import IcHand from 'icons/safety-landing/Hand.svg'
+import IcFraud from 'icons/safety-landing/Fraud.svg'
+import IcLink from 'icons/safety-landing/Link.svg'
+import IcPin from 'icons/safety-landing/Pin.svg'
+import IcSms from 'icons/safety-landing/Sms.svg'
+import IcYes from 'icons/safety-landing/Yes.svg'
+import IcNo from 'icons/safety-landing/No.svg'
 import Logo from '../Logo'
 import LanguageSelect from '../LanguageSelect'
 import PrimaryButton from '../Buttons/PrimaryButton'
@@ -23,9 +32,9 @@ const SafetyLayout: FC = observer(() => {
           <span className='text-h-3'>{t('SECURITY_ON_ADVERTO_TEXT')}</span>
           <span className='text-h-3 '>{t('SECURITY_ON_ADVERTO_TEXT_BR')}</span>
         </div>
-        <PrimaryButton className='mx-4 mt-15'>
-          {t('REPORT_FRAUD')}
-        </PrimaryButton>
+        {/* <PrimaryButton className='mx-4 mt-15'> */}
+        {/*  {t('REPORT_FRAUD')} */}
+        {/* </PrimaryButton> */}
       </div>
       <div className='flex flex-col pt-15 mx-4'>
         <h1 className='text-h-1 font-medium mb-9'>
@@ -49,7 +58,128 @@ const SafetyLayout: FC = observer(() => {
             __html: t('LANDING_KEEP_SECRET_PERSONAL_DATA_RULES'),
           }}
         />
-        <div className=''>123</div>
+        <div className='space-y-6 mb-15'>
+          <div className='flex'>
+            <IcImportant className='min-w-[16px] h-4 mr-4' />
+            <span className='text-body-1'>
+              {t('LANDING_KEEP_SECRET_PERSONAL_DATA_RULES_1')}
+            </span>
+          </div>
+          <div className='flex'>
+            <IcImportant className='min-w-[16px] h-4 mr-4' />
+            <span className='text-body-1'>
+              {t('LANDING_KEEP_SECRET_PERSONAL_DATA_RULES_3')}
+            </span>
+          </div>
+          <div className='flex'>
+            <IcImportant className='min-w-[16px] h-4 mr-4' />
+            <span className='text-body-1'>
+              {t('LANDING_KEEP_SECRET_PERSONAL_DATA_RULES_4')}
+            </span>
+          </div>
+          <div className='flex'>
+            <IcImportant className='min-w-[16px] h-4 mr-4' />
+            <span className='text-body-1'>
+              {t('LANDING_KEEP_SECRET_PERSONAL_DATA_RULES_5')}
+            </span>
+          </div>
+        </div>
+        <IcDeliveryFast className='w-8 h-8 mb-4' />
+        <span className='text-headline-5 font-medium mb-2'>
+          {t('LANDING_DELIVERY')}
+        </span>
+        <span
+          className='text-body-1 mb-15'
+          dangerouslySetInnerHTML={{
+            __html: t('LANDING_DELIVERY_TEXT'),
+          }}
+        />
+        <IcHand className='w-8 h-8 mb-4' />
+        <span className='text-headline-5 font-medium mb-2'>
+          {t('LANDING_MEET_PERSONALLY')}
+        </span>
+        <span
+          className='text-body-1 mb-15'
+          dangerouslySetInnerHTML={{
+            __html: t('LANDING_MEET_PERSONALLY_TEXT'),
+          }}
+        />
+        <IcFraud className='w-8 h-8 mb-4' />
+        <span className='text-headline-5 font-medium mb-2'>
+          {t('LANDING_AVOID_STRANGERS')}
+        </span>
+        <span
+          className='text-body-1 mb-15'
+          dangerouslySetInnerHTML={{
+            __html: t('LANDING_AVOID_STRANGERS_TEXT'),
+          }}
+        />
+      </div>
+      <div className='flex flex-col bg-[#FFF6EE] pt-12 pb-15'>
+        <div className='mx-4 flex flex-col items-center'>
+          <h1 className='text-h-1 font-medium text-center mb-15'>
+            {t('LANDSCAPE_DO NOT_DECEIVE_YOUSELF')}
+          </h1>
+          <IcLink className='w-25 h-25 mb-6' />
+          <span className='text-h-2 font-medium text-center mb-2'>
+            {t('LANDING_CHECK_LINK')}
+          </span>
+          <span className='text-body-1 text-center mb-15'>
+            {t('LANDING_CHECK_LINK_TEXT')}
+          </span>
+
+          <IcPin className='w-25 h-25 mb-6' />
+          <span className='text-h-2 font-medium text-center mb-2'>
+            {t('LANDING_DO NOT_ENTER_PIN')}
+          </span>
+          <span className='text-body-1 text-center mb-15'>
+            {t('LANDING_DO NOT_ENTER_PIN_TEXT')}
+          </span>
+
+          <IcSms className='w-25 h-25 mb-6' />
+          <span className='text-h-2 font-medium text-center mb-2'>
+            {t('LANDING_SMS_WITH_CODE')}
+          </span>
+          <span className='text-body-1 text-center'>
+            {t('LANDING_SMS_WITH_CODE_TEXT')}
+          </span>
+        </div>
+      </div>
+      <div className='flex flex-col py-15 mx-4'>
+        <h1 className='text-headline-5 font-medium mb-2'>
+          {t('LENDING_FRAUDERS_CAN_BE_SELLERS_AND_BUYERS')}
+        </h1>
+        <span className='text-body-1 mb-15'>{t('LANDING_DELIVERY_TEXT')}</span>
+        <h1 className='text-headline-5 font-medium mb-2'>
+          {t('LANDING_MAKE_SURE_YOU_ON_ADVERTO')}
+        </h1>
+        <span className='text-body-1 mb-15'>
+          {t('LANDING_MAKE_SURE_YOU_ON_ADVERTO_TEXT')}
+        </span>
+        <div className='nc-gradient-brand w-full rounded-lg py-7 mb-7 flex flex-col items-center'>
+          <IcYes className='w-13 h-13 mb-5' />
+          <span className='text-h-3 text-white mb-3 font-medium'>
+            {t('LANDING_CORRECT_SITES')}
+          </span>
+          <span
+            className='text-body-2 text-white mb-3 whitespace-pre	leading-7'
+            dangerouslySetInnerHTML={{
+              __html: t('LANDING_CORRECT_SITES_TEXT'),
+            }}
+          />
+        </div>
+        <div className='bg-nc-salmon w-full rounded-lg py-7 flex flex-col items-center'>
+          <IcNo className='w-13 h-13 mb-5' />
+          <span className='text-h-3 mb-3 font-medium'>
+            {t('LANDING_FAKE_SITES')}
+          </span>
+          <span
+            className='text-body-2 mb-3 whitespace-pre	leading-7'
+            dangerouslySetInnerHTML={{
+              __html: t('LANDING_FAKE_SITES_TEXT'),
+            }}
+          />
+        </div>
       </div>
     </div>
   )
--- pages/safety.tsx
+++ pages/safety.tsx
@@ -1,8 +1,6 @@
 import {serverSideTranslations} from 'next-i18next/serverSideTranslations'
 import {GetServerSideProps} from 'next'
-import {getLocationCodes, processCookies} from '../helpers'
-import {fetchCountries} from '../api/v1'
-import {fetchCategories} from '../api/v2'
+import {processCookies} from '../helpers'
 import SafetyLayout from '../components/Layouts/SafetyLayout'
 
 export default function Home() {
@@ -12,32 +10,9 @@ export default function Home() {
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const state = await processCookies(ctx)
 
-  const promises = [
-    fetchCountries(state.language),
-    fetchCategories(state.language),
-  ]
-
-  const [countriesData, categoriesData] = await Promise.allSettled(
-    promises,
-  ).then((res) =>
-    res.map((p) => (p.status === 'fulfilled' ? p.value : p.reason)),
-  )
-  const categories = categoriesData?.result ?? null
-
-  const countries = countriesData ?? null
   return {
     props: {
-      hydrationData: {
-        categoriesStore: {
-          categories,
-        },
-        countriesStore: {
-          countries,
-        },
-        generalStore: {
-          locationCodes: getLocationCodes(ctx),
-        },
-      },
+      hydrationData: {},
       ...(await serverSideTranslations(state.language)),
     },
   }
