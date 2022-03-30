a7d23f724d68b338f31f84830890bba7ad899478
danyasnov <yasnov.one@gmail.com>
2022-03-30 15:22:20 +0300

    safety landing 768PX

--- components/Layouts/SafetyLayout.tsx
+++ components/Layouts/SafetyLayout.tsx
@@ -25,23 +25,25 @@ const SafetyLayout: FC = observer(() => {
   return (
     <div>
       <MetaTags title={t('SECURITY_ON_ADVERTO')} />
-      <div className='flex justify-between items-center my-2.5 mx-4'>
+      <div className='flex justify-between items-center my-2.5 mx-4 s:mx-15 s:my-7'>
         <Logo />
         <LanguageSelect />
       </div>
       <div className='flex flex-col bg-[#FFF6EE] pt-8 pb-18'>
-        <IcPeople className='w-[240px] h-[291px] mb-15 self-center' />
-        <div className='mx-4 flex flex-col space-y-4'>
-          <h1 className='text-h-1 font-medium'>{t('SECURITY_ON_ADVERTO')}</h1>
+        <IcPeople className='w-[240px] s:w-[309px] h-[291px] s:h-[375px] mb-15 s:mb-18 self-center' />
+        <div className='mx-4 s:mx-15 flex flex-col space-y-4'>
+          <h1 className='text-h-1 s:text-headline-2 font-medium'>
+            {t('SECURITY_ON_ADVERTO')}
+          </h1>
           <span className='text-h-3'>{t('SECURITY_ON_ADVERTO_TEXT')}</span>
           <span className='text-h-3 '>{t('SECURITY_ON_ADVERTO_TEXT_BR')}</span>
         </div>
-        {/* <PrimaryButton className='mx-4 mt-15'> */}
+        {/* <PrimaryButton className='mx-4 mt-15 s:mt-18'> */}
         {/*  {t('REPORT_FRAUD')} */}
         {/* </PrimaryButton> */}
       </div>
-      <div className='flex flex-col pt-15 mx-4'>
-        <h1 className='text-h-1 font-medium mb-9'>
+      <div className='flex flex-col pt-15 s:pt-18 mx-4 s:mx-15'>
+        <h1 className='text-h-1 s:text-headline-2 font-medium mb-9'>
           {t('LANDING_GENERAL_SAFETY_RULES')}
         </h1>
         <IcLanguage className='w-8 h-8 mb-4' />
@@ -49,7 +51,7 @@ const SafetyLayout: FC = observer(() => {
           {t('LANDING_CHECK_SITE')}
         </span>
         <span
-          className='text-body-1 mb-15'
+          className='text-body-1 mb-15 s:mb-18'
           dangerouslySetInnerHTML={{__html: t('LANDING_CHECK_SITE_TEXT')}}
         />
         <IcVerified className='w-8 h-8 mb-4' />
@@ -57,12 +59,12 @@ const SafetyLayout: FC = observer(() => {
           {t('LANDING_KEEP_SECRET_PERSONAL_DATA')}
         </span>
         <span
-          className='text-body-1 mb-15'
+          className='text-body-1 mb-15 s:mb-18'
           dangerouslySetInnerHTML={{
             __html: t('LANDING_KEEP_SECRET_PERSONAL_DATA_RULES'),
           }}
         />
-        <div className='space-y-6 mb-15'>
+        <div className='space-y-6 mb-15 s:mb-18'>
           <div className='flex'>
             <IcImportant className='min-w-[16px] h-4 mr-4' />
             <span className='text-body-1'>
@@ -93,7 +95,7 @@ const SafetyLayout: FC = observer(() => {
           {t('LANDING_DELIVERY')}
         </span>
         <span
-          className='text-body-1 mb-15'
+          className='text-body-1 mb-15 s:mb-18'
           dangerouslySetInnerHTML={{
             __html: t('LANDING_DELIVERY_TEXT'),
           }}
@@ -103,7 +105,7 @@ const SafetyLayout: FC = observer(() => {
           {t('LANDING_MEET_PERSONALLY')}
         </span>
         <span
-          className='text-body-1 mb-15'
+          className='text-body-1 mb-15 s:mb-18'
           dangerouslySetInnerHTML={{
             __html: t('LANDING_MEET_PERSONALLY_TEXT'),
           }}
@@ -113,22 +115,22 @@ const SafetyLayout: FC = observer(() => {
           {t('LANDING_AVOID_STRANGERS')}
         </span>
         <span
-          className='text-body-1 mb-15'
+          className='text-body-1 mb-15 s:mb-18'
           dangerouslySetInnerHTML={{
             __html: t('LANDING_AVOID_STRANGERS_TEXT'),
           }}
         />
       </div>
-      <div className='flex flex-col bg-[#FFF6EE] pt-12 pb-15'>
-        <div className='mx-4 flex flex-col items-center'>
-          <h1 className='text-h-1 font-medium text-center mb-15'>
+      <div className='flex flex-col bg-[#FFF6EE] pt-12 pb-15 s:pb-18'>
+        <div className='mx-4 s:mx-15 flex flex-col items-center'>
+          <h1 className='text-h-1 s:text-headline-2 font-medium text-center mb-15 s:mb-18'>
             {t('LANDSCAPE_DO NOT_DECEIVE_YOUSELF')}
           </h1>
           <IcLink className='w-25 h-25 mb-6' />
           <span className='text-h-2 font-medium text-center mb-2'>
             {t('LANDING_CHECK_LINK')}
           </span>
-          <span className='text-body-1 text-center mb-15'>
+          <span className='text-body-1 text-center mb-15 s:mb-18'>
             {t('LANDING_CHECK_LINK_TEXT')}
           </span>
 
@@ -136,7 +138,7 @@ const SafetyLayout: FC = observer(() => {
           <span className='text-h-2 font-medium text-center mb-2'>
             {t('LANDING_DO NOT_ENTER_PIN')}
           </span>
-          <span className='text-body-1 text-center mb-15'>
+          <span className='text-body-1 text-center mb-15 s:mb-18'>
             {t('LANDING_DO NOT_ENTER_PIN_TEXT')}
           </span>
 
@@ -149,49 +151,51 @@ const SafetyLayout: FC = observer(() => {
           </span>
         </div>
       </div>
-      <div className='flex flex-col py-15 mx-4'>
+      <div className='flex flex-col py-15 s:py-18 mx-4 s:mx-15'>
         <h1 className='text-headline-5 font-medium mb-2'>
           {t('LENDING_FRAUDERS_CAN_BE_SELLERS_AND_BUYERS')}
         </h1>
-        <span className='text-body-1 mb-15'>{t('LANDING_DELIVERY_TEXT')}</span>
+        <span className='text-body-1 mb-15 s:mb-18'>
+          {t('LANDING_DELIVERY_TEXT')}
+        </span>
         <h1 className='text-headline-5 font-medium mb-2'>
           {t('LANDING_MAKE_SURE_YOU_ON_ADVERTO')}
         </h1>
-        <span className='text-body-1 mb-15'>
+        <span className='text-body-1 mb-15 s:mb-18'>
           {t('LANDING_MAKE_SURE_YOU_ON_ADVERTO_TEXT')}
         </span>
         <div className='nc-gradient-brand w-full rounded-lg py-7 mb-7 flex flex-col items-center'>
-          <IcYes className='w-13 h-13 mb-5' />
-          <span className='text-h-3 text-white mb-3 font-medium'>
+          <IcYes className='w-13 h-13 s:w-25 s:h-25 mb-5' />
+          <span className='text-h-3 s:text-h-1 s:mb-10 text-white mb-3 font-medium'>
             {t('LANDING_CORRECT_SITES')}
           </span>
           <span
-            className='text-body-2 text-white mb-3 whitespace-pre	leading-7'
+            className='text-body-2 s:text-headline-6 text-white mb-3 whitespace-pre	leading-7'
             dangerouslySetInnerHTML={{
               __html: t('LANDING_CORRECT_SITES_TEXT'),
             }}
           />
         </div>
         <div className='bg-nc-salmon w-full rounded-lg py-7 flex flex-col items-center'>
-          <IcNo className='w-13 h-13 mb-5' />
-          <span className='text-h-3 mb-3 font-medium'>
+          <IcNo className='w-13 h-13 s:w-25 s:h-25 mb-5' />
+          <span className='text-h-3 s:text-h-1 mb-3 s:mb-10 font-medium'>
             {t('LANDING_FAKE_SITES')}
           </span>
           <span
-            className='text-body-2 mb-3 whitespace-pre	leading-7'
+            className='text-body-2 s:text-headline-6 mb-3 whitespace-pre leading-7'
             dangerouslySetInnerHTML={{
               __html: t('LANDING_FAKE_SITES_TEXT'),
             }}
           />
         </div>
       </div>
-      <div className='flex flex-col bg-[#FFF6EE] py-15'>
-        <div className='mx-4 flex flex-col items-center'>
-          <h1 className='text-h-1 font-medium text-center mb-15'>
+      <div className='flex flex-col bg-[#FFF6EE] py-15 s:py-18'>
+        <div className='mx-4 s:mx-15 flex flex-col items-center'>
+          <h1 className='text-h-1 font-medium text-center mb-15 s:mb-18'>
             {t('LENDING_FRAUDERS_CAN_BE_SELLERS_AND_BUYERS')}
           </h1>
-          <div className='flex flex-col bg-nc-salmon py-4 mb-7'>
-            <span className='text-headline-5 font-medium text-center'>
+          <div className='flex flex-col bg-nc-salmon py-4 mb-7 s:w-screen'>
+            <span className='text-headline-5 s:text-headline-6 font-medium text-center'>
               {t('LENDING_SELLER_CAN_DECEIVE')}
             </span>
           </div>
@@ -202,14 +206,14 @@ const SafetyLayout: FC = observer(() => {
               alt='seller fraud'
             />
           </div>
-          <div className='w-[328px] h-[231px] relative mb-7'>
+          <div className='w-[328px] h-[231px] relative mb-7 s:mb-15'>
             <ImageWrapper
               layout='fill'
               type='/img/safety-landing/seller-fraud-2.png'
               alt='seller fraud'
             />
           </div>
-          <h1 className='text-h-1 font-medium text-center mb-15'>
+          <h1 className='text-h-1 font-medium text-center mb-15 s:mb-18'>
             {t('LANDING_HOW_TO_RECOGNIZE_FRAUD_SELLER')}
           </h1>
           <span
@@ -218,7 +222,7 @@ const SafetyLayout: FC = observer(() => {
               __html: t('LANDING_HOW_TO_RECOGNIZE_FRAUD_SELLER_TEXT'),
             }}
           />
-          <div className='flex flex-col bg-nc-salmon py-4 my-15'>
+          <div className='flex flex-col bg-nc-salmon py-4 my-15 s:my-18 s:w-screen'>
             <span className='text-headline-5 font-medium text-center'>
               {t('LENDING_BUYER_CAN_DECEIVE')}
             </span>
@@ -244,7 +248,7 @@ const SafetyLayout: FC = observer(() => {
               alt='seller fraud'
             />
           </div>
-          <h1 className='text-h-1 font-medium text-center my-15'>
+          <h1 className='text-h-1 font-medium text-center my-15 s:my-18'>
             {t('LANDING_HOW_TO_RECOGNIZE_FRAUD_BUYER')}
           </h1>
           <span
@@ -255,25 +259,25 @@ const SafetyLayout: FC = observer(() => {
           />
         </div>
       </div>
-      <div className='flex flex-col py-15 mx-4'>
+      <div className='flex flex-col py-15 s:py-18 mx-4 s:mx-15'>
         <h1 className='text-h-1 font-medium mb-10'>
           {t('LANDING_MAKE_THE_WORLD_PLEASER')}
         </h1>
         <div className='flex'>
           <span className='w-4 h-4 rounded-full bg-nc-primary flex-shrink-0 mr-4' />
-          <span className='text-body-1 mb-15'>
+          <span className='text-body-1 mb-8'>
             {t('LANDING_MAKE_THE_WORLD_PLEASER_TEXT_1')}
           </span>
         </div>
         <div className='flex'>
           <span className='w-4 h-4 rounded-full bg-nc-primary flex-shrink-0 mr-4' />
-          <span className='text-body-1 mb-15'>
+          <span className='text-body-1 mb-8'>
             {t('LANDING_MAKE_THE_WORLD_PLEASER_TEXT_2')}
           </span>
         </div>
         <div className='flex'>
           <span className='w-4 h-4 rounded-full bg-nc-primary flex-shrink-0 mr-4' />
-          <span className='text-body-1 mb-15'>
+          <span className='text-body-1 mb-8'>
             {t('LANDING_MAKE_THE_WORLD_PLEASER_TEXT_3')}
           </span>
         </div>
