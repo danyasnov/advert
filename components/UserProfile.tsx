292c967ff0487209f21d20f2c93603e43b52d29b
danyasnov <yasnov.one@gmail.com>
2022-08-26 14:30:58 +0300

    Карточка профиля не соответствует дизайну в личном кабинете

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -18,26 +18,26 @@ const UserProfile: FC = observer(() => {
           size={40}
         />
       </div>
-      <span className='mb-3 text-body-14 text-green text-center font-semibold'>
+      <span className='mb-3 text-body-14 text-green text-center'>
         {t('ONLINE')}
       </span>
       <span className='text-h-5 text-greyscale-900 font-bold mb-8 break-words'>
         {user.name}
       </span>
       <div className='flex justify-between mb-8 w-full'>
-        <div className='flex flex-wrap w-1/2 justify-center'>
-          <span className='text-body-14 text-greyscale-900 font-semibold mr-2'>
+        <div className='flex s:flex-col s:text-center w-1/2 justify-center'>
+          <span className='text-body-14 m:text-body-18 text-greyscale-900 font-semibold mr-2'>
             {user.subscribers}
           </span>
-          <span className='text-body-14 text-greyscale-900 font-normal'>
+          <span className='text-body-14 m:text-body-16 text-greyscale-900 font-normal'>
             {t('SUBSCRIBERS')}
           </span>
         </div>
-        <div className='flex flex-wrap w-1/2 justify-center whitespace-pre'>
-          <span className='text-body-14 text-greyscale-900 font-semibold mr-2'>
+        <div className='flex s:flex-col s:text-center w-1/2 justify-center whitespace-pre'>
+          <span className='text-body-14 m:text-body-18 text-greyscale-900 font-semibold mr-2'>
             {user.subscribs}
           </span>
-          <span className='text-body-14 text-greyscale-900 font-normal'>
+          <span className='text-body-14 m:text-body-16 text-greyscale-900 font-normal'>
             {t('SUBSCRIPTIONS')}
           </span>
         </div>
