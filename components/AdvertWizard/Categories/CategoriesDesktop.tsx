a7ea3a38282827c6287eac5a1d236664d6e282c5
danyasnov <yasnov.one@gmail.com>
2022-09-16 12:07:53 +0300

    Дизайн списка категорий полностью не соответствует дизайну в создании объявления

--- components/AdvertWizard/Categories/CategoriesDesktop.tsx
+++ components/AdvertWizard/Categories/CategoriesDesktop.tsx
@@ -1,6 +1,6 @@
 import {Dispatch, FC, SetStateAction} from 'react'
 import {CACategoryModel} from 'front-api/src/index'
-import {isEmpty} from 'lodash'
+import {isEmpty, size} from 'lodash'
 import Button from '../../Buttons/Button'
 import ImageWrapper from '../../ImageWrapper'
 
@@ -42,43 +42,38 @@ const CategoriesDesktop: FC<Props> = ({selected, setSelected, categories}) => {
   return (
     <>
       <div className='flex mb-15'>
-        <div className='grid grid-cols-3 w-full gap-x-4'>
-          {selected.map((parentCategory, index) => (
-            <div
-              className='flex flex-col'
-              data-test-id={`parent-category-${parentCategory.id}`}
-              key={parentCategory.id}>
-              {parentCategory.items.map((c) => (
-                <Button
-                  key={c.id}
-                  id={`parent-category-item-${c.id}`}
-                  className={`min-h-10 hover:bg-nc-accent rounded-lg py-2 px-4 ${
-                    selected[index + 1]?.id === c.id ? 'bg-nc-accent' : ''
-                  }`}
-                  onClick={() => {
-                    if (selected.length - 1 === index) {
-                      setSelected([...selected, c])
-                    } else {
-                      setSelected([...selected.slice(0, index + 1), c])
-                    }
-                  }}>
-                  <span className='text-greyscale-900 text-body-16 font-normal w-full text-left'>
-                    {c.name}
-                  </span>
-                </Button>
-              ))}
-            </div>
-          ))}
-        </div>
-        <div className='ml-4 hidden l:block'>
-          <ImageWrapper
-            type={`/img/categories/${selected[0].slug}.png`}
-            width={290}
-            height={290}
-            alt={selected[0].name}
-            className='rounded-xl'
-            layout='fixed'
-          />
+        <div className='grid grid-cols-3 m:grid-cols-5 w-full divide-x divide-greyscale-100'>
+          {selected.map(
+            (parentCategory, index) =>
+              !isEmpty(parentCategory.items) && (
+                <div
+                  className='flex flex-col'
+                  data-test-id={`parent-category-${parentCategory.id}`}
+                  key={parentCategory.id}>
+                  {parentCategory.items.map((c) => (
+                    <Button
+                      key={c.id}
+                      id={`parent-category-item-${c.id}`}
+                      className={`min-h-10 hover:text-primary-500 text-greyscale-900 rounded-lg py-2 px-4 ${
+                        selected[index + 1]?.id === c.id
+                          ? 'text-primary-500'
+                          : ''
+                      }`}
+                      onClick={() => {
+                        if (selected.length - 1 === index) {
+                          setSelected([...selected, c])
+                        } else {
+                          setSelected([...selected.slice(0, index + 1), c])
+                        }
+                      }}>
+                      <span className='text-body-14 font-normal w-full text-left'>
+                        {c.name}
+                      </span>
+                    </Button>
+                  ))}
+                </div>
+              ),
+          )}
         </div>
       </div>
     </>
