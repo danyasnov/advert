55fdb3022730d1ecde1c7c57ad911d858e8d3540
danyasnov <yasnov.one@gmail.com>
2022-10-07 20:46:05 +0300

    Не закрывается элемент ввода диапазонов при повторном нажатии на стрелку на странице фильтров

--- components/FormikComponents.tsx
+++ components/FormikComponents.tsx
@@ -636,7 +636,11 @@ export const FormikRange: FC<FieldProps & IFormikRange> = ({
             </span>
           )}
 
-          <IcArrowDown className='fill-current text-greyscale-900 h-5 w-5 -mr-2' />
+          <IcArrowDown
+            className={`fill-current text-greyscale-900 h-5 w-5 -mr-2 ${
+              show ? 'rotate-180' : ''
+            }`}
+          />
         </div>
       </Button>
       {show && (
