a8bc552de06e2d296fa0baca6d4ca03c99d0248a
Diana <ArshinovaDE@gmail.com>
2022-10-18 13:53:59 +0300

    Сделаны стили в OutlineButton

--- components/Buttons/OutlineButton.tsx
+++ components/Buttons/OutlineButton.tsx
@@ -4,14 +4,14 @@ import Button from './Button'
 interface Props {
   children?: ReactNode
   className?: string
-  onClick: () => void
+  onClick?: () => void
   id?: string
 }
 const OutlineButton: FC<Props> = ({children, className, onClick, id}) => {
   return (
     <Button
       id={id}
-      className={`px-6 py-4 border border-greyscale-200 rounded-2xl text-greyscale-900 text-body-16 font-semibold ${
+      className={`px-8 py-4.5 border border-greyscale-200 rounded-2xl background-white text-greyscale-900 text-body-16 font-semibold leading-5.5 ${
         className || ''
       }`}
       onClick={onClick}>
