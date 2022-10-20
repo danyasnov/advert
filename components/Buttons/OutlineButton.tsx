ca45f26572d847da4f19bdb466015010726b89ff
Diana <ArshinovaDE@gmail.com>
2022-10-20 15:01:38 +0300

    Добавлена тень кнопке

--- components/Buttons/OutlineButton.tsx
+++ components/Buttons/OutlineButton.tsx
@@ -11,7 +11,7 @@ const OutlineButton: FC<Props> = ({children, className, onClick, id}) => {
   return (
     <Button
       id={id}
-      className={`px-8 py-4.5 border border-greyscale-200 rounded-2xl background-white text-greyscale-900 text-body-16 font-semibold ${
+      className={`px-8 py-4.5 border border-greyscale-200 rounded-2xl background-white text-greyscale-900 text-body-16 font-semibold shadow-lg ${
         className || ''
       }`}
       onClick={onClick}>
