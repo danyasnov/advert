816bddc21205fda44c6943a2c09a36cd3ad0f382
danyasnov <yasnov.one@gmail.com>
2022-10-20 12:54:16 +0300

    Затемнить проданные и заблокированные объявления в личном кабинете

--- components/Cards/CardBadge.tsx
+++ components/Cards/CardBadge.tsx
@@ -38,7 +38,7 @@ const CardBadge: FC<Props> = ({state}) => {
   }
 
   return (
-    <div className='absolute w-full z-10 flex justify-center top-1/4 '>
+    <div className='absolute w-full z-20 flex justify-center top-1/4 '>
       <span
         className={`${color} flex rounded-md text-body-14 text-white px-2.5 py-1.5`}>
         {t(message)}
