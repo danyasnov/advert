c38bf0110a9f71c14234c3f5ce385798c63f12b0
Diana <ArshinovaDE@gmail.com>
2022-12-01 15:55:23 +0200

    добавлены кнопки для бизнеса и безопасности

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,6 +12,8 @@ import {useGeneralStore} from '../providers/RootStoreProvider'
 import useDisableBodyScroll from '../hooks/useDisableBodyScroll'
 import LanguageSelect from './LanguageSelect'
 import Button from './Buttons/Button'
+import BusinessButton from './BusinessButton'
+import SafetyButton from './SafetyButton'
 
 const Header: FC = observer(() => {
   const {push} = useRouter()
@@ -22,9 +24,15 @@ const Header: FC = observer(() => {
   return (
     <header className='flex s:justify-center relative z-10'>
       <div className='header-width'>
-        <div className='flex s:justify-between px-4 s:px-0 py-2 mb-2'>
-          <div className='flex  w-full s:w-auto space-x-4 items-center'>
+        <div className='flex justify-start s:space-between pl-4 s:pl-0 py-2 mb-2'>
+          <div className='flex  w-full s:w-auto space-x-4 items-center cursor-pointer'>
             <LanguageSelect />
+          </div>
+          <div className='flex  w-full mr-[26px] s:w-auto space-x-4 items-center'>
+            <BusinessButton />
+          </div>
+          <div className='flex  w-full mr-6 s:w-auto space-x-4 items-center'>
+            <SafetyButton />
           </div>
         </div>
         <div className='flex pt-2 pb-5 px-4 s:px-0 space-x-4 s:space-x-6 m:space-x-8'>
---
+++ components/SafetyButton.tsx
@@ -0,0 +1,23 @@
+import {FC} from 'react'
+import {useTranslation} from 'next-i18next'
+import {Danger} from 'react-iconly'
+import LinkWrapper from './Buttons/LinkWrapper'
+
+const SafetyButton: FC = () => {
+  const {t} = useTranslation()
+  return (
+    <LinkWrapper
+      title='security'
+      href='/safety'
+      className='flex justify-center items-center cursor-pointer '>
+      <div className='mr-2 text-primary-500'>
+        <Danger set='light' size={20} />
+      </div>
+      <div className='text-greyscale-900 text-body-12 font-medium'>
+        {t('SECURITY')}
+      </div>
+    </LinkWrapper>
+  )
+}
+
+export default SafetyButton
