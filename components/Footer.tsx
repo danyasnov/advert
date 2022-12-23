d785136af18901c810abe933f33278cd3944c58c
Diana <ArshinovaDE@gmail.com>
2022-12-23 17:39:23 +0200

    добавлена кнопка в футер

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -54,16 +54,16 @@ const Footer: FC = observer(() => {
         </div>
         <div className='border-t border-shadow-b pt-2'>
           <div className='flex flex-col m:flex-row justify-between  mx-4 s:mx-auto'>
-            <div className='flex flex-col space-y-5 my-8 s:my-0 s:flex-row s:space-y-0 s:space-x-10 text-greyscale-900 text-body-14 font-semibold text-left'>
+            <div className='flex flex-col space-y-5 my-8 s:my-0 s:flex-row s:space-y-0 s:flex-wrap text-greyscale-900 text-body-14 font-medium m:font-semibold text-left'>
               <LinkWrapper
                 title={t('TERMS_AND_CONDITIONS')}
-                className='flex justify-center items-center whitespace-nowrap'
+                className='flex s:mr-10 justify-center items-center whitespace-nowrap'
                 href='/p/terms-and-conditions'>
                 {t('TERMS_AND_CONDITIONS')}
               </LinkWrapper>
               <LinkWrapper
                 title={t('PRIVACY_POLICY')}
-                className='flex justify-center items-center whitespace-nowrap'
+                className='flex s:mr-10 justify-center items-center whitespace-nowrap'
                 href='/p/privacy-policy'>
                 {t('PRIVACY_POLICY')}
               </LinkWrapper>
@@ -75,16 +75,22 @@ const Footer: FC = observer(() => {
               {/*  {t('RULES')} */}
               {/* </LinkWrapper> */}
               <LinkWrapper
-                className='flex justify-center items-center whitespace-nowrap'
+                className='flex s:mr-10 justify-center items-center whitespace-nowrap'
                 title={t('COOKIES_POLICY')}
                 href='/p/cookies-policy'>
                 {t('COOKIES_POLICY')}
               </LinkWrapper>
               <LinkWrapper
-                className='flex justify-center items-center w-full'
+                className='flex m:mr-10 justify-center items-center'
                 title={t('SUPPORT')}
                 href='/support'>
                 {t('SUPPORT')}
+              </LinkWrapper>
+              <LinkWrapper
+                className='flex s:mr-10 justify-center items-center whitespace-nowrap'
+                title={t('FOR_BUSINESS')}
+                href='/business'>
+                {t('FOR_BUSINESS')}
               </LinkWrapper>
             </div>
             <div className='text-body-14 font-normal mb-8 s:my-7 text-greyscale-900 self-start'>
