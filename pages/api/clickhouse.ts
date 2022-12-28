e60ddd7ac953ed246e726b797ba7b32c9f2c3287
danyasnov <yasnov.one@gmail.com>
2022-12-28 19:28:32 +0700

    clickhouse

---
+++ pages/api/clickhouse.ts
@@ -0,0 +1,18 @@
+import {parseCookies} from 'nookies'
+import type {NextApiRequest, NextApiResponse} from 'next'
+import {fetchCities} from '../../api/v1'
+import {handleClickHouse} from '../../api/v2'
+import {SerializedCookiesState} from '../../types'
+
+export default (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+  const {body} = req
+  const cookies: SerializedCookiesState = parseCookies({req})
+  return handleClickHouse({
+    eventType: body.eventType,
+    sessionId: cookies.sessionId,
+    token: cookies.authNewToken,
+    data: body.data,
+  }).then((response) => {
+    res.json(response)
+  })
+}
