19d01eca7c640f76f8741ebc5090fac99721497e
Diana <ArshinovaDE@gmail.com>
2023-01-17 15:14:46 +0200

    new header

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,43 +2,18 @@ import {FC} from 'react'
 import LinkWrapper from './Buttons/LinkWrapper'
 import ImageWrapper from './ImageWrapper'
 
-interface Props {
-  variant?: 'small' | 'big' | 'both'
-}
-const Logo: FC<Props> = ({variant = 'both'}) => {
-  let body = null
-  const small = (
-    <ImageWrapper
-      type='/img/logo/ShortLogo.svg'
-      alt='Logo'
-      width={40}
-      height={40}
-      layout='fixed'
-    />
+const Logo: FC = () => {
+  const body = (
+    <div className='h-10'>
+      <ImageWrapper
+        type='/img/logo/FullLogo.svg'
+        alt='Logo'
+        width={172}
+        height={40}
+        layout='fixed'
+      />
+    </div>
   )
-  const big = (
-    <ImageWrapper
-      type='/img/logo/FullLogo.svg'
-      alt='Logo'
-      width={172}
-      height={40}
-      layout='fixed'
-    />
-  )
-  if (variant === 'both') {
-    body = (
-      <>
-        <div className='m:block hidden h-10'>{big}</div>
-        <div className='m:hidden h-10 w-10'>{small}</div>
-      </>
-    )
-  }
-  if (variant === 'small') {
-    body = <div className='h-10 w-10'>{small}</div>
-  }
-  if (variant === 'big') {
-    body = <div className='h-10'>{big}</div>
-  }
   return (
     <LinkWrapper
       title='logo'
