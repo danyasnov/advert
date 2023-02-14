1f916b52bfdc417c5a59f75325b406e2d9f37123
danyasnov <yasnov.one@gmail.com>
2023-02-14 13:32:57 +0700

    remove next plugins

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-const withPlugins = require('next-compose-plugins')
 const withTM = require('next-transpile-modules')(
   [
     'front-api',
@@ -68,8 +67,6 @@ const nextConfig = {
     ]
   },
 }
+const plugins = [withTM, withBundleAnalyzer, withSentryConfig]
 
-module.exports = withPlugins(
-  [withTM, withBundleAnalyzer, [withSentryConfig, {silent: true}]],
-  nextConfig,
-)
+module.exports = plugins.reduce((acc, next) => next(acc), nextConfig)
