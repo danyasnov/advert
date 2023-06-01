dfb63c8d006bdf0d2fb73a68461873a490e6dcf8
Diana <ArshinovaDE@gmail.com>
2023-06-01 13:53:24 +0300

    поисковая строка стран в моб версии отсутствует

--- components/Selects/ListWithFilter.tsx
+++ components/Selects/ListWithFilter.tsx
@@ -1,12 +1,14 @@
 import React, {FC, useState} from 'react'
 import {useTranslation} from 'next-i18next'
 import IcSearch from 'icons/material/Search.svg'
+import {Search} from 'react-iconly'
 import List, {IList} from './List'
 
 const ListWithFilter: FC<IList & {isSearchable?: boolean}> = (props) => {
   const {items, isMulti, onChange, value, isSearchable} = props
   const {t} = useTranslation()
   const [filtered, setFiltered] = useState(items)
+  const [focused, setFocused] = useState(false)
   const [filter, setFilter] = useState('')
   const onClose = () => {
     setFilter('')
@@ -16,12 +18,20 @@ const ListWithFilter: FC<IList & {isSearchable?: boolean}> = (props) => {
   return (
     <div className='w-full flex flex-col h-full'>
       {isSearchable && (
-        <div className='w-full px-4 relative mt-px'>
-          <IcSearch className='w-6 h-6 absolute top-3 left-5 text-greyscale-800' />
+        <div className='w-full px-4 relative mt-6'>
+          {/* <IcSearch className='w-5 h-5 absolute top-2.5 left-6 text-greyscale-400' /> */}
+          <div
+            className={`absolute top-2.5 left-8 ${
+              focused ? 'text-primary-500' : 'text-greyscale-400'
+            } `}>
+            <Search set='light' size={20} />
+          </div>
           <input
-            className='w-full h-12 border border-nc-border flex rounded-lg py-4 pr-4 pl-8 text-greyscale-900'
+            className='w-full h-10 border border-nc-border flex rounded-xl py-4 pr-4 pl-11 text-greyscale-900'
             placeholder={t('SEARCH')}
             value={filter}
+            onFocus={() => setFocused(true)}
+            onBlur={() => setFocused(false)}
             onChange={({target}) => {
               setFilter(target.value)
               setFiltered(
