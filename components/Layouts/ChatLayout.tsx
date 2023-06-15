6489d0e74fee2b14d0ae84f372054cac9159c537
danyasnov <yasnov.one@gmail.com>
2023-06-15 13:39:31 +0300

    fixes

--- components/Layouts/ChatLayout.tsx
+++ components/Layouts/ChatLayout.tsx
@@ -1,24 +1,8 @@
-import {FC, useEffect, useRef, useState} from 'react'
+import {FC} from 'react'
 import {observer} from 'mobx-react-lite'
 import {useTranslation} from 'next-i18next'
-import {parseCookies} from 'nookies'
-import {toJS} from 'mobx'
-import {Chats, globalChatsStore, chatEventEmitter, ChatData} from 'chats'
-import {useRouter} from 'next/router'
-import {isEmpty, size} from 'lodash'
-import {Delete} from 'react-iconly'
-import {SerializedCookiesState} from '../../types'
-import {useGeneralStore} from '../../providers/RootStoreProvider'
-import HeaderFooterWrapper from './HeaderFooterWrapper'
-import {getRest} from '../../api'
-import Storage from '../../stores/Storage'
 import Logo from '../Logo'
-import {handleMetrics} from '../../helpers'
 import Auth from '../Auth'
-import ImageWrapper from '../ImageWrapper'
-import EmptyProductImage from '../EmptyProductImage'
-import {unixMlToDate} from '../../utils'
-import Button from '../Buttons/Button'
 import ChatList from '../Chat/ChatList'
 import MetaTags from '../MetaTags'
 
@@ -28,7 +12,7 @@ const ChatLayout: FC = observer(() => {
   return (
     <div className='flex flex-col py-6 px-4 s:px-8'>
       <MetaTags title={t('MESSAGES')} />
-      <div className='flex pb-2 w-full justify-between'>
+      <div className='flex pb-2 s:pb-8 w-full justify-between'>
         <Logo />
         <h4 className='text-h-4 font-bold text-greyscale-900 hidden s:flex self-center w-full ml-6'>
           {t('MESSAGES')}
