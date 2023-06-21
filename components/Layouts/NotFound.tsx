c8651e7901e0cf7acb994fe8661411abb8b62700
Diana <ArshinovaDE@gmail.com>
2023-06-21 12:21:09 +0300

    change color name

--- components/Layouts/NotFound.tsx
+++ components/Layouts/NotFound.tsx
@@ -15,7 +15,7 @@ const NotFound: FC = observer(() => {
   return (
     <>
       <MetaTags title={t('PAGE_ERROR')} />
-      <div className='bg-blue'>
+      <div className='bg-dark-blue'>
         <Header />
         <div className='flex flex-col s:flex-row s:justify-between items-center mx-4 s:pt-8 s:mx-auto s:w-[704px] m:w-[944px] l:w-[1210px]'>
           <div className='flex flex-col items-center s:items-start mt-10'>
