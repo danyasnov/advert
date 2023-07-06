0ccb654d4012ff39ce1c64c1023457932d604c59
danyasnov <yasnov.one@gmail.com>
2023-07-06 13:45:41 +0300

    fix prod layout width

--- components/Layouts/ProductLayout.tsx
+++ components/Layouts/ProductLayout.tsx
@@ -176,7 +176,7 @@ const ProductLayout: FC = observer(() => {
         <div className='m:flex m:mx-12 m:justify-center m:w-full'>
           <div className='m:w-944px l:w-[1208px] mx-4 s:mx-8 m:mx-0'>
             <div className='s:flex s:space-x-4 m:space-x-8'>
-              <main className='s:w-full m:w-[614px] l:w-896px'>
+              <main className='s:w-4/6 m:w-[614px] l:w-896px'>
                 <ProductHeader getOptions={getOptions(16)} />
                 <ProductPhotos />
                 <ProductDescription getOptions={getOptions(24)} />
